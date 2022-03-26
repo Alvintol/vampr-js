95e05820402412104d1118b89a4d6ff2557adeac
Alvin <alvin.tol@hotmail.com>
2022-03-26 02:33:44 +0000

    implement some simple, non-recursive tree node methods

--- vampire.js
+++ vampire.js
@@ -10,21 +10,46 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVamp = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numOfVamp++;
+    }
+    return numOfVamp
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } return false;
+  }
+
+   // Returns the vampire object with that name, or null if no vampire exists with that name
+   vampireWithName(name) {
+
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
 
   }
 
@@ -36,7 +61,23 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
+    if (this.isMoreSeniorThan(vampire)) {
+      return this;
+    }
+    if (this.numberOfVampiresFromOriginal === 1) {
+      return this.creator;
+    }
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfOffspring) {
+      return this.creator;
+    }
+    if (this.creator === vampire.creator) {
+      return this.creator;
+    }
 
+    return vampire;
   }
 }
 
